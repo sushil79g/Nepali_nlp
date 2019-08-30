a4e649039849caa0f34fc0d9731af4163e0d2ce4
Sushil Ghimire <sushil79g@gmail.com>
2019-08-30 11:30:30 +0545

    add load_fasttext embedding function

--- WordEmbedding/fasttext_embedding.py
+++ WordEmbedding/fasttext_embedding.py
@@ -1,2 +1,12 @@
 import wget
-def load_fasttext(link="")
+import gzip
+from gensim.models.keyedvectors import KeyedVectors
+def load_fasttext(link="https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.ne.300.vec.gz"):
+    fil_ = wget.download(link)
+    print('embedding_downloaded')
+    abc = gzip.open(fil_, 'rb')
+    file_content = abc.read()
+    abc.close()
+    model = KeyedVectors.load_word2vec_format(file_content, binary=False)
+    model.save_word2vec_format('wiki.np.txt',binary=False)
+    return model
