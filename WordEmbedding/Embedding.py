9e2d1b2cd68e69700d56b725e4bf39c5771f2a32
Sushil Ghimire <sushil79g@gmail.com>
2019-08-30 10:44:30 +0545

    add load_embedding custom

---
+++ WordEmbedding/Embedding.py
@@ -0,0 +1,14 @@
+import gensim
+from gensim.models.keyedvectors import KeyedVectors
+
+def load_vector():
+    """Download and load embedding. #This is private embedding I will update link later
+    
+    Returns:
+        [keyedVectors] -- [Custom Nepali word Embedding]
+    """
+    from google.colab import drive
+    drive.mount('/content/drive')
+    base_folder = 'drive/My Drive/dataset/NepaliEmbed/'
+    word_vector = KeyedVectors.load(base_folder + 'word_vector.sg')
+    return word_vector
