160c84171836cddadd3ec7205769f41c4c0c8d6f
Anish Pandey <35564460+sharmaanix@users.noreply.github.com>
2019-08-30 12:59:28 +0545

    Update Embedding.py
    Fix typo

--- WordEmbedding/Embedding.py
+++ WordEmbedding/Embedding.py
@@ -1,9 +1,8 @@
 import gensim
 from gensim.models.keyedvectors import KeyedVectors
+
 #soon S3 bucket link will be provided.This is just for test
-def load_vector():​
-4
-#Soon S
+def load_vector():
     """Download and load embedding. #This is private embedding I will update link later
     
     Returns:
@@ -13,4 +12,6 @@ def load_vector():​
     drive.mount('/content/drive')
     base_folder = 'drive/My Drive/dataset/NepaliEmbed/'
     word_vector = KeyedVectors.load(base_folder + 'word_vector.sg')
+    
     return word_vector
+
