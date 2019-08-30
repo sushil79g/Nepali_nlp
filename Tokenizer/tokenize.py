2d2af940aeaf31079a0503b978a14e1d3f8fa136
Sushil Ghimire <sushil79g@gmail.com>
2019-08-30 10:38:41 +0545

    add sentence tokenizer

---
+++ Tokenizer/tokenize.py
@@ -0,0 +1,18 @@
+import string
+
+def sentence_tokenize(text):
+    """This function tokenize the sentences
+    
+    Arguments:
+        text {string} -- Sentences you want to tokenize
+    
+    Returns:
+        sentence {list} -- tokenized sentence in list
+    """
+    sentences = text.split(u"।")
+    sentences = [sentence.translate(str.maketrans('', '', string.punctuation)) for sentence in sentences]
+    return sentences
+
+def word_tokenize(sentece):
+    punctuations = ['।',',',';','?','!','—','-']
+    
