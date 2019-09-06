bb4b00dd796b26e1e12f534d33b407a273304309
Sushil Ghimire <sushil79g@gmail.com>
2019-09-06 12:22:09 +0545

    add function to download files from google drive

--- nepali_nlp/Embedding.py
+++ nepali_nlp/Embedding.py
@@ -1,5 +1,6 @@
 import gensim
 from gensim.models.keyedvectors import KeyedVectors
+from nepali_nlp.Download_embedding import download_file_from_google_drive
 
 #soon S3 bucket link will be provided.This is just for test
 def load_vector():
@@ -8,10 +9,8 @@ def load_vector():
     Returns:
         [keyedVectors] -- [Custom Nepali word Embedding]
     """
-    from google.colab import drive
-    drive.mount('/content/drive')
-    base_folder = 'drive/My Drive/dataset/NepaliEmbed/'
-    word_vector = KeyedVectors.load(base_folder + 'word_vector.sg')
+    download_file_from_google_drive('1ik38vahOmzhiU2DBi78VOqDt7YFPsk5w','word_vector.sg')
+    word_vector = KeyedVectors.load('word_vector.sg')
     
     return word_vector
 
@@ -21,10 +20,8 @@ def load_large_vector():
     Returns:
         [keyedVectors] -- [Custom Nepali word Embedding]
     """
-    from google.colab import drive
-    drive.mount('/content/drive')
-    base_folder = 'drive/My Drive/'
-    word_vector = KeyedVectors.load(base_folder + 'nepali_embeddings_word2vec.txt')
+    download_file_from_google_drive('1KnAZ2Eeqwz3S9VrAuzTLWysAaRB6Ch7e','nepali_embeddings_word2vec.txt')
+    word_vector = KeyedVectors.load('nepali_embeddings_word2vec.txt')
 
     return word_vector
 
