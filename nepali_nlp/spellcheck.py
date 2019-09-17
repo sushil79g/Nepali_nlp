beb0603f530d6e1b884f3eacf3c9b83a40935c7e
sushil ghimire <sushil79g@gmail.com>
2019-09-17 21:59:58 +0545

    update bug in spellchecker

--- nepali_nlp/spellcheck.py
+++ nepali_nlp/spellcheck.py
@@ -1,18 +1,19 @@
+# coding: utf-8
 import sys 
 sys.path.append('..')
 
 from difflib import get_close_matches
 
 def load_words(location ='../local_dataset/words.txt'):
-    file = open(location, 'r', encoding="utf8")
+    file = open(location, 'r')
     text = file.read()
-    txt = text.split()
+    text = text.split()
     file.close()
     return text
 
 def corrector(word,number=2, threshold = 0.3):
     word_list = load_words()
-    matches = get_close_matches(word, word_list, n=number, cutoff=threshold)
+    matches = get_close_matches(word, word_list, n=2, cutoff=0.3)
+
     return matches
 
-print(corrector('वह्किल'))
