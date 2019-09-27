34bd676cea7acb990ca39ce4f28c8e6d75ff838c
Sushil Ghimire <sushil79g@gmail.com>
2019-09-27 11:10:57 +0545

    add centroid generating function using tfidf

--- nepali_nlp/summerization.py
+++ nepali_nlp/summerization.py
@@ -2,8 +2,23 @@ from Embedding import Embeddings
 from sklearn.feature_extraction.text import TfidfVectorizer
 class Summerize:
     def __init__(self, *args, **kwargs):
-        pass
+        # this should not be the stander way of doing.
+        print('loading embedding')
+        self.word_vec = Embedding().load_vector()
+        self.vocab = word_vec.vocab
+        print('everything loaded')
 
     def generate_centroid_tfidf(self, sentence):
         tf = TfidfVectorizer()
+        tfidf = tf.fit_transfor(sentenct).toarray().sum(0)
+        tfidf = np.divide(tfidf, tfidf.max())
+        words = tf.get_feature_names()
+        similar_term = []
+
+        for i in range(len(tfidf)):
+            if words[i] in self.vocab:
+                if tfidf[i] >=0.2:
+                    similar_term.append(words[i])
         
+        res = [self.word_vec[term] for term in similar_term]
+        return sum(res)/len(res)
