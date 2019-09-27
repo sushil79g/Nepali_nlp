b6b51953689800a2f33b87b154d229c6333b2b8f
Sushil Ghimire <sushil79g@gmail.com>
2019-09-27 11:14:47 +0545

    add sentence vectorizer

--- nepali_nlp/summerization.py
+++ nepali_nlp/summerization.py
@@ -1,5 +1,6 @@
 from Embedding import Embeddings
 from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 class Summerize:
     def __init__(self, *args, **kwargs):
         # this should not be the stander way of doing.
@@ -22,3 +23,19 @@ class Summerize:
         
         res = [self.word_vec[term] for term in similar_term]
         return sum(res)/len(res)
+
+    def sentence_vectorizer(sentence,size):
+        dic = {}
+        for i in range(len(sentence)):
+            sum_vec = np.zeros(size)
+            try:
+                sentence_word = sentence[i].split()
+            except:
+                pass
+            sentence = [word for word in sentence_word if word in self.vocab]
+            if sentence:
+                for word in sentence:
+                    word_vec_ = self.word_vec[word]
+                    sum_vec = np.add(sum_vec, word_vec_)
+                dic[i] = sum_vec/len(sentence)
+        return dic
