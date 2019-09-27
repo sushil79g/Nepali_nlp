f5c11284af517194fe9046d6c6e5fd3e2227eed2
Sushil Ghimire <sushil79g@gmail.com>
2019-09-27 13:33:31 +0545

    add 'n' sentence summary for Nepali text

--- nepali_nlp/summerization.py
+++ nepali_nlp/summerization.py
@@ -1,30 +1,34 @@
 from Embedding import Embeddings
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+import string
 class Summerize:
     def __init__(self, *args, **kwargs):
         # this should not be the stander way of doing.
-        print('loading embedding')
-        self.word_vec = Embedding().load_vector()
-        self.vocab = word_vec.vocab
-        print('everything loaded')
+        print('Loading Embedding')
+        self.word_vec = Embeddings().load_vector()
+        self.vocab = self.word_vec.vocab
+        print('Embedding is now loaded')
 
-    def generate_centroid_tfidf(self, sentence):
+    def preprocess(self,text):
+        sentences = text.split(u"।")
+        sentences = [sentence.translate(str.maketrans('', '', string.punctuation)) for sentence in sentences]
+        return sentences
+    
+    def generate_centroif_tfidf(self,sentence):
         tf = TfidfVectorizer()
-        tfidf = tf.fit_transfor(sentenct).toarray().sum(0)
+        tfidf = tf.fit_transform(sentence).toarray().sum(0)
         tfidf = np.divide(tfidf, tfidf.max())
         words = tf.get_feature_names()
         similar_term = []
-
         for i in range(len(tfidf)):
             if words[i] in self.vocab:
-                if tfidf[i] >=0.2:
+                if tfidf[i] >= 0.2:
                     similar_term.append(words[i])
-        
         res = [self.word_vec[term] for term in similar_term]
         return sum(res)/len(res)
 
-    def sentence_vectorizer(sentence,size):
+    def sentence_vectorizer(self,sentence,size):
         dic = {}
         for i in range(len(sentence)):
             sum_vec = np.zeros(size)
@@ -37,27 +41,27 @@ class Summerize:
                 for word in sentence:
                     word_vec_ = self.word_vec[word]
                     sum_vec = np.add(sum_vec, word_vec_)
+                    
                 dic[i] = sum_vec/len(sentence)
         return dic
 
-    def sentence_selection(centroid, sentences_dict, summary_length):
+    def sentence_selection(self,centroid, sentences_dict, summary_length):
         from scipy.spatial.distance import cosine
         sentence_retriever = []
         record = []
-
         for sentence_id in sentences_dict:
             vector = sentences_dict[sentence_id]
-            similarity = (1- cosine(centroid, vector))
+            similarity = (1 - cosine(centroid, vector))
             record.append((sentence_id, vector, similarity))
 
-        rank = list(reversed(sorted(record, key= lambda tup: tup[2])))
+        rank = list(reversed(sorted(record, key=lambda tup: tup[2])))
         sentence_ids = []
-        summary_char_num = 0 
+        summary_char_num = 0
         stop = False
-        i = 0 
-        text_length = sum( [len(x) for x in sentence_retriever])
+        i = 0
+        text_length = sum([len(x) for x in sentence_retriever])
 
-        if summary_length <1:
+        if summary_length < 1:
             limit = int(text_length * float(summary_length))
 
             while not stop and i < len(rank):
@@ -65,23 +69,35 @@ class Summerize:
                 new_vector = sentences_dict[sentence_id]
                 sent_char_num = len(sentence_retriever[sentence_id])
                 redundancy = [sentences_dict[k] for k in sentence_ids
-                             if (1- cosine(new_vector, sentences_dict[k]) > 0.95)]
-                
+                                if (1 - cosine(new_vector, sentences_dict[k]) > 0.95)]
+
                 if not redundancy:
                     summary_char_num += sent_char_num
                     sentence_ids.append(sentence_id)
-                
-                i = i + 1
+                i += 1
 
                 if summary_char_num > limit:
                     stop = True
-                
-        else:
+        else: 
             sentences_number = int(summary_length)
             sentence_ids = rank[:sentences_number]
-            sentence_ids = map(lambda t:t[0], sentence_ids)
-        
-        sentence_ids = sorted(sentence_ids)
+            sentence_ids = map(lambda t: t[0], sentence_ids)
 
+        sentence_ids = sorted(sentence_ids)
+        
         return sentence_ids
 
+    def combine_sentence(self,centroid_tfidf,sent,sent_dict,length):
+        ids = self.sentence_selection(centroid_tfidf,sent_dict,length)
+        whole_summary = []
+        for inde in ids:
+            whole_summary.append(sent[inde])
+        return '।'.join(whole_summary)
+
+
+    def show_summary(self,sample_text,length_sentence_predict):
+        sent = self.preprocess(sample_text)
+        centroid_tfidf = self.generate_centroif_tfidf(sent)
+        size = self.word_vec.vector_size
+        sent_dict = self.sentence_vectorizer(sent,size)
+        return self.combine_sentence(centroid_tfidf,sent,sent_dict,length_sentence_predict)
