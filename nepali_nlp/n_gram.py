aebec8de070272818c3b4f8fd3df407f38ab092f
sharmaanix <anis.95@yahoo.com>
2019-11-15 12:27:43 +0545

    Add test case

--- nepali_nlp/n_gram.py
+++ nepali_nlp/n_gram.py
@@ -20,3 +20,14 @@ class NgramGenerator(Tokenizer):
         for idx in range(len(token_text)-self.n_gram+1):
             n_gram_wordlist.append(token_text[idx:idx+self.n_gram])
         return n_gram_wordlist
+
+
+#Test Case
+# data = "तिमीले सुन्या यो कुरा हो, मैले सुन्या को कुरा हो ,तिम्रो हाम्रो सम्बन्ध को हल्ला चल्या को कुरा हो ,झ्याम्म झ्याम्म।"
+# one_gram = NgramGenerator(1)
+# two_gram = NgramGenerator(2)
+# three_gram = NgramGenerator(3)
+
+# print(one_gram.generate_n_gram(one_gram.word_tokenize(data)))
+# print(two_gram.generate_n_gram(two_gram.word_tokenize(data)))
+# print(three_gram.generate_n_gram(three_gram.word_tokenize(data)))
