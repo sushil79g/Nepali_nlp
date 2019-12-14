5ecb1c06c119ffae20698608d3286e5c4ce49229
sushil <sushil79g@gmail.com>
2019-12-14 22:56:52 +0545

    add the docstring

--- nepali_nlp/news_latest.py
+++ nepali_nlp/news_latest.py
@@ -7,16 +7,28 @@ class Update_news:
         pass
 
     def show_latest(self, portal='onlinekhabar', number_of_news=5):
+        """his function returns tile of latest news, link for latest news and summerize news 
+        
+        Keyword Arguments:
+            portal {str} -- [news portal sites; for now either 'onlinekhabar' or 'ekantipur'] (default: {'onlinekhabar'})
+            number_of_news {int} -- [Number of top trending news] (default: {5})
+        
+        Returns:
+            [tuple] -- [tuple of (titles, links, news_summerises)]
+        """
         assert portal in ['onlinekhabar','ekantipur'], "we currently support only ekantipur and onlinekhabar"
         extracted_link = top_news_link(portal=portal, top_n=number_of_news)
         summar_ = Summerize()
-        for link_ in extract_news:
+        links = []
+        titles = []
+        news_summerises = []
+        for link in extracted_link:
+            
             title,text = extract_news(link)
-            print(link_)
-            print(title)
             summary_news = summar_.show_summary(text,7)
-            print(summary_news)
-            print(' ')
-        
-        return 0
+            links.append(link)
+            titles.append(title)
+            news_summerises.append(summary_news)
+            
+        return (titles, links, news_summerises)
         
