1a0af6b8752f6b53356ddb42cdba90ca0dd79431
sushil ghimire <sushil79g@gmail.com>
2020-10-07 12:47:57 +0545

    add nepali text stemming

---
+++ Nepali_nlp/stemmer.py
@@ -0,0 +1,21 @@
+import snowballstemmer
+
+class Stem:
+    """Stem the words to its root eg 'गरेका' to 'गर'."""
+    def __init__(self) -> None:
+        self.stemmer = snowballstemmer.NepaliStemmer()
+    
+    def rootify(self, text):
+        """Generates the stem words for input text.
+
+        Args:
+            text (Union(List, str)): Text to be stemmed or lemmatized.
+
+        Returns:
+            Union(List, str): stemmed text.
+        """
+        if isinstance(text, str):
+            return self.stemmer.stemWords(text.split())
+        
+        return self.stemmer.stemWords(text)
+        
